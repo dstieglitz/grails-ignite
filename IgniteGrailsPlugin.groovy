eab38ae6c14416b97ff01117e6c6a6d7d2ffc969
dstieglitz <dstieglitz@stainlesscode.com>
2015-07-14 19:23:04 -0700

    version 0.2.0

--- IgniteGrailsPlugin.groovy
+++ IgniteGrailsPlugin.groovy
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException
 
 class IgniteGrailsPlugin {
     // the plugin version
-    def version = "0.2-SNAPSHOT"
+    def version = "0.2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging

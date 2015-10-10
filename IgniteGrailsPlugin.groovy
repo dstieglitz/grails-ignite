d22d82c0d5940bc7834c4df1c252a91e4dd2801a
dstieglitz <dstieglitz@stainlesscode.com>
2015-10-10 10:37:30 -0700

    bumped minor version

--- IgniteGrailsPlugin.groovy
+++ IgniteGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.grails.ignite.IgniteStartupHelper
 
 class IgniteGrailsPlugin {
     // the plugin version
-    def version = "0.3.0-SNAPSHOT"
+    def version = "0.3.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging

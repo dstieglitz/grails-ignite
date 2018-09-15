77a524da41c448fba0b4af0b29fbd6789b193fc0
dstieglitz <dstieglitz@stainlesscode.com>
2018-09-15 08:53:43 -0700

    SNAPSHOT version

--- IgniteGrailsPlugin.groovy
+++ IgniteGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.grails.ignite.TaskDecorator
 
 class IgniteGrailsPlugin {
     // the plugin version
-    def version = "1.3.1"
+    def version = "1.3.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
